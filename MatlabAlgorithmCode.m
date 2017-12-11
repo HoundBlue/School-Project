4bb7b793205c4a31a6bd1c366d546ba9e16bbdc9
HoundBlue <32943331+HoundBlue@users.noreply.github.com>
2017-12-11 08:04:55 -0500

    Update MatlabAlgorithmCode.m

--- MatlabAlgorithmCode.m
+++ MatlabAlgorithmCode.m
@@ -12,111 +12,83 @@ legitimate reader id the same during testing
 
  
 
-genforTesting =
-randi(9007199254740991);
+genforTesting = randi(9007199254740991);
 
 extraBits = 1+(randi(2046));
 
-queryID =
-uint64((genforTesting*extraBits)); %This one will be the input used
-for queries 
+queryID = uint64((genforTesting*extraBits)); %This one will be the input used
+%for queries 
 
  %from unauthorized readers
+ %For Testing:
+  %Unauthorized Readers:
 
-    %For Testing:
+%A. Tag ID = 4567, ReaderID = 45678 
 
-    %Unauthorized Readers:
+%B. Tag ID = 12345, ReaderID = 67890 
 
-%A. Tag ID = 4567, Reader
-ID = 45678 
+%C. Tag ID = 87654, ReaderID = 908762 
 
-%B. Tag ID = 12345, Reader
-ID = 67890 
+%D. Tag ID = 1, Reader ID = 99999 
 
-%C. Tag ID = 87654, Reader
-ID = 908762 
-
-%D. Tag ID = 1, Reader ID =
-99999 
-
-%E. Tag ID = 902135, Reader
-ID = 23452 
+%E. Tag ID = 902135, ReaderID = 23452 
 
  
-
-%Generate a random number
-from API of 0 to 18,446,744,073,709,551,615 
+%Generate a random numberfrom API of 0 to 18,446,744,073,709,551,615 
 
 
 maxRand = 18446744073709551615;
 
 minRand = 65535;
 
-a =
-(randi(9007199254740991)*extraBits);
+a = (randi(9007199254740991)*extraBits);
 
-    %to create a base range of at least
-16 bits
+    %to create a base range of at least 16 bits
 
 randoNum=uint64(minRand+a);
 
-%Figure out what the return
-number will be r' or new Rando
+%Figure out what the return number will be r' or new Rando
 
 rShift = sum(dec2bin(randoNum)=='1');
 
-rPrime =
-bitshift(randoNum,rShift);
+rPrime = bitshift(randoNum,rShift);
 
  
 
-%Xor the random number with
-the readerID within the API
+%Xor the random number with the readerID within the API
 
 doXor = bitxor(readerID,randoNum);
 
  
+%RFID reader aspect and functions
 
-%RFID reader aspect and
-functions
+%send query to the tag from the reader
 
-%send query to the tag from
-the reader
+%query = doXor; % for now but switch to = queryID for unauthorized
 
-%query = doXor; % for now
-but switch to = queryID for unauthorized
-
-query = queryID; %use
-instead for unauthorized reader tests
+query = queryID; %use instead for unauthorized reader tests
 
  
 
-%RFID Tag aspect and
-functions
+%RFID Tag aspect and functions
 
-%tag recieves query flag
-yes no
+%tag recieves query flag yes no
 
  
 
-%xor query with readerID
-stored in memory to get random number
+%xor query with readerID stored in memory to get random number
 
-randoTest =
-bitxor(readerID,query);% finds r
+randoTest = bitxor(readerID,query);% finds r
 
  
-
-%test if random number is
-within range
+%test if random number is within range
 
 if ((minRand<=randoTest)
 && (randoTest<=maxRand)) 
 
     disp('query is within range')
 
-%Perform counting of 1's
-traversing the array since it is within range
+%Perform counting of 1's traversing the array since it is within range
 
 onesCount = dec2bin(randoTest);
 
@@ -124,42 +96,30 @@ onesCount = dec2bin(randoTest);
 
     N = sum(onesCount=='1');    
 
-    % N equals number of 1's in the
-randomnumber
+    % N equals number of 1's in the randomnumber
 
   
 shiftRando = uint64(randoTest);
 
-    %shift randotest N bits to the
-left
+    %shift randotest N bits to the left
 
-    newRando = bitshift(shiftRando,N); % new
-rando is rr
+    newRando = bitshift(shiftRando,N); % new rando is rr
 
     
 
-    %xor the new random number from
-the bitshift with the TagID
+    %xor the new random number from the bitshift with the TagID
 
   
 returnQuery = bitxor(newRando,tagID); % This is sent to the API
 
   
-
-
-  
 %API needs to do xor to find the tagID
 
   
-findTag = bitxor(rPrime,returnQuery); %should give the API 
-
-  
-%the correct tag ID
+findTag = bitxor(rPrime,returnQuery); %should give the API the correct tag ID
 
   
 rfidTag = uint64(findTag);
-
-  
 
 
 else
